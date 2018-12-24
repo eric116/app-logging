0dd71c77cb95d1ae94b4d94ddee9fe3a08191911
Eric Carlson <carlsoneric@protonmail.com>
2018-12-23 23:09:53 -0600

    First version that works for some programs

---
+++ setup-logging.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "What is the name of the application you wish to log? (ex. firefox-esr, cura-lulzbot, nautilus, etc.)"
+read varApp
+echo "Great, where shall we stash the logs for "$varApp"?"
+read -p "Enter file path ["$HOME"/logs/"$varApp"]:" varFilepath
+varFilepath=${filepath:-$HOME/logs/$varApp}
+echo "Coolio, I'll stash the logs in "$varFilepath"."
+if [ ! -d $varFilepath ]; then
+	echo $varFilepath" not found, so I'll create it."
+	mkdir -p $varFilepath
+else
+	echo $varFilepath" exists, so I'll use that."
+fi
+
+if [ ! -d $HOME/bin ]; then
+	echo $HOME/bin" not found, so I'll create it and put the executable there."
+	mkdir -p $HOME/bin
+else
+	echo $HOME"/bin exits, so I'll put the executable there."
+fi
+cd $HOME/bin
+printf "#!/bin/sh \nscript $varFilepath/\`date +\"%%Y%%m%%d%%H%%M%%S\"\`.log -f -c $varApp" > log-$varApp.sh
+chmod a+rx log-$varApp.sh
